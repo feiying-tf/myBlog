7ebb7865bbb63b461e8c3fcf2f686468a29f440e
tangfeng <462787828@qq.com>
2022-10-15 21:31:38 +0800

    fix: 添加http相关内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,15 +4,16 @@ const fs = require('fs')
 const path = require('path')
 const moment = require('moment')
 module.exports = {
-  title: '面试知识点',
+  title: '前端知识',
   description: '记录前端面试中比较重要的知识点',
-  // theme: 'reco',
+  theme: 'reco', // 使用reco主题
   base: '/myBlog/', // 由于项目的根地址是：https://tfeng-use.github.io/myBlog/，所以必须加上base才整正确访问到对应的assets资源
-  devServer: {
-    https: true,
-    key: fs.readFileSync(path.resolve(__dirname, './localhost+1-key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, './localhost+1.pem')),
-  },
+  // https开启了以后，就不能通过 http://localhost:8080/myBlog/ 直接进行访问了
+  // devServer: {
+  //   https: true,
+  //   key: fs.readFileSync(path.resolve(__dirname, './localhost+1-key.pem')),
+  //   cert: fs.readFileSync(path.resolve(__dirname, './localhost+1.pem')),
+  // },
   locales: {
     '/': {
       lang: 'zh-CN', // 可以保证 date 显示按照年/月/日进行显示
@@ -22,7 +23,7 @@ module.exports = {
     // displayAllHeaders: false,
     sidebarDepth: 0,
     lastUpdated: '上次更新', // 默认是关闭的，在给定一个字符串后，它会作为前缀显示
-    // subSidebar: 'auto', // 将多级标题放在右侧，生成子侧边栏
+    subSidebar: 'auto', // 将多级标题放在右侧，生成子侧边栏
     // 设置导航栏
     nav: [
       { text: 'github', link: 'https://github.com/tfeng-use' },
@@ -40,19 +41,25 @@ module.exports = {
     // 添加侧边栏
     sidebar: [
       {
-        title: '欢迎学习',
+        title: '基础知识',
         // path: '/',
         collapsable: false,
         children: [{ title: '学前必读', path: '/' }],
       },
       {
-        title: '基础学习',
+        title: '前端面试',
         // path: '/handbook/ConditionalTypes',
         collapsable: false,
         children: [
-          { title: '条件类型', path: '/handbook/ConditionalTypes' },
+          { title: 'http', path: '/handbook/http' },
           { title: '泛型', path: '/handbook/Generics' },
         ],
+      },
+      {
+        title: '技能树',
+        // path: '/handbook/ConditionalTypes',
+        collapsable: false,
+        children: [{ title: '条件类型', path: '/newTechTree/1' }],
       },
     ],
   },
